72ffde9187e6e6369e8b921c80f8c650c7157d45
Samestora <putrantosurya38@gmail.com>
2025-10-09 17:09:17 +0700

    Init : All Device Targeted but mainly Mobile

---
+++ README.md
@@ -0,0 +1,10 @@
+# Penuhan
+
+A Continuation of Kosongan
+
+## Getting Started
+Ya gitulah, kalau mau dev gini
+
+```bash
+flutter pub get
+```
---
+++ android/app/src/main/kotlin/com/sgdc/penuhan/penuhan/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sgdc.penuhan.penuhan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
